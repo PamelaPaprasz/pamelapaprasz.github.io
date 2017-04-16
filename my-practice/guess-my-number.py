83974ca75e59c54b2c9922d26009687ebc630afa
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-17 00:04:19 +0200

    refact user input

--- my-practice/guess-my-number.py
+++ my-practice/guess-my-number.py
@@ -7,20 +7,19 @@ chance_to_guess = 3
 hit = False
     
 while not hit and chance_to_guess > 0:
-    print('You have ' + str(chance_to_guess) + ' chances left.')
-    print('Guess a number: ')
+    print('You have ', chance_to_guess, ' chances left.')
     
-    guess = int(input())
+    guess = int(input('Guess a number: '))
     hit = my_fav_num == guess
     
     if hit:
-        print('You guessed it! My favorite number is: ' + str(my_fav_num))
+        print('You guessed it! My favorite number is: ', my_fav_num)
             
     elif chance_to_guess > 1:
         print('Your guess is not my favorite number, try again.')
-        
+
     else:
-        print('No more chance to guess')
+        print('Game is over. You could not guess my favorite number and you have no more chance to left.')
             
     chance_to_guess -= 1
     
