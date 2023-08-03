aaa28ff366fc1094dcebf74799265e63f4c697fe
paprasz <papraszpamela@gmail.com>
2023-08-03 18:25:22 +0200

    initializing a react app and add basic app structure

---
+++ my-app/src/pages/Fun.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Fun() {
+  return (
+    <div>
+      <h1>Fun Page</h1>
+      <p>Welcome to the what I have learnt section.</p>
+    </div>
+  );
+}
+
+export default Fun;
