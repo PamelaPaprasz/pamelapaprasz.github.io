4b6f1de92bc6981e252e5e14b038720b64e71e51
paprasz <papraszpamela@gmail.com>
2023-08-05 01:49:45 +0200

    restructure 2

---
+++ src/pages/Work.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Work() {
+  return (
+    <div>
+      <h1>Work Page</h1>
+      <p>Welcome to the what I have learnt section.</p>
+    </div>
+  );
+}
+
+export default Work;
