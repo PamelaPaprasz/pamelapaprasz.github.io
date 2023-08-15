5118c42c4fd6eeff8f0be2f635a646786bc172bb
paprasz <papraszpamela@gmail.com>
2023-08-15 18:58:50 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ScrollToTopOnNavigation from './ScrollToTopOnNavigation';
-import Navbar from './Navbar';
+import ScrollToTopOnNavigation from './helper/ScrollToTopOnNavigation';
+import Navbar from './elements/Navbar';
+import Footer from './elements/Footer';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Fun from './pages/Fun';
@@ -20,6 +21,7 @@ function App() {
         <Route path="/fun" element={<Fun />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/elements/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { AiFillLinkedin, AiFillGithub, AiOutlineMail } from 'react-icons/ai';
+
+function Footer() {
+  return (
+    <footer className="container mx-auto bg-transparent text-darkBeige p-4 flex justify-between w-full">
+      <div className="text-left w-full"> {/* Add a container to center-align the content */}
+        © 2023 Pamela Paprasz. All rights reserved.
+      </div>
+      <div className="w-full mx-auto flex justify-end items-right">
+        <a href="mailto:your.email@example.com" className="mr-4">
+            <AiOutlineMail size={24} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/paprasz" // Update with your LinkedIn profile URL
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mr-4"
+        >
+          <AiFillLinkedin size={24} />
+        </a>
+        <a
+          href="https://github.com/pamelapaprasz" // Update with your GitHub profile URL
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <AiFillGithub size={24} />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
+
